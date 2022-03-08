7005728bd00daedb7f8c7860eabf1e7172e02864
btolstukha <btolstukha@moysklad.ru>
2022-03-08 10:43:53 +0600

    cleanup

--- migration/migrations/001.do.create-table-airport.sql
+++ migration/migrations/001.do.create-table-airport.sql
@@ -11,10 +11,10 @@ create table planetype
 
 comment on table planetype is 'types of planes';
 
-insert into planetype (id, type_name, speed)
-values (uuid_generate_v4(), 'B733', 430),
-       (uuid_generate_v4(), 'PAY2', 230),
-       (uuid_generate_v4(), 'C500', 250),
-       (uuid_generate_v4(), 'B738', 440),
-       (uuid_generate_v4(), 'PA34', 130),
-       (uuid_generate_v4(), 'A320', 420);
+INSERT INTO planetype (id, type_name, speed) VALUES ('1e90ef56-d5c2-4a5e-973d-13043ac78ac5', 'B733', 430);
+INSERT INTO planetype (id, type_name, speed) VALUES ('39f05ee2-37d0-414c-9cd0-b403be7bb16a', 'PAY2', 230);
+INSERT INTO planetype (id, type_name, speed) VALUES ('36123658-36e7-4e99-91f9-d3ca9e84b8e1', 'C500', 250);
+INSERT INTO planetype (id, type_name, speed) VALUES ('f7c87abc-1a9e-4e9a-9196-310bc77c755c', 'B738', 440);
+INSERT INTO planetype (id, type_name, speed) VALUES ('7881c792-449b-4eda-9320-8c29ab3e086b', 'PA34', 130);
+INSERT INTO planetype (id, type_name, speed) VALUES ('513319b8-c1ad-451b-b7a4-add0e05d443d', 'A320', 420);
+
